f3aeaa98e82d1049485ed6054eb4b82b75adefe4
Ömer Atılım Koca <93714072+atilimkoca@users.noreply.github.com>
2026-02-01 00:27:21 +0300

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -19,7 +19,7 @@ export default ({ config }) => ({
   ios: {
     ...config.ios,
     bundleIdentifier: 'com.zenithstudio.app',
-    buildNumber: '5',
+    buildNumber: '6',
     supportsTablet: true,
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
