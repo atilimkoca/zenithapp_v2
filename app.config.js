263f65654a2dc7afc6abd604e47101e2c4bd6b7f
Ömer Atılım Koca <93714072+atilimkoca@users.noreply.github.com>
2026-02-01 00:59:42 +0300

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default ({ config }) => ({
   ...config,
   name: 'Zenith Studio',
   slug: config.slug || 'zenith-studio',
-  version: '1.0.3',
+  version: '1.0.4',
   orientation: 'portrait',
   icon: './assets/app_icon.jpeg',
   userInterfaceStyle: 'light',
